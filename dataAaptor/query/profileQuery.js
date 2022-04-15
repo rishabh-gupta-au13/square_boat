d97ee94c521bbe3f4f2c5075b026a39df1999046
rishabh-gupta-au13 <rgp02071993@gmail.com>
2022-04-15 22:34:26 +0530

    added save user logic

--- dataAaptor/query/profileQuery.js
+++ dataAaptor/query/profileQuery.js
@@ -1,40 +1,35 @@
-const customerModel=require("../models/customers")
+const customerModel = require("../models/customers");
 class profileQuery {
-    async checkEmail(email,mobileNumber){
-        try{
-            let emailCheck=await customerModel.find({$or:[{email:email},{mobileNumber:mobileNumber}]})
-            if(emailCheck.length>1){
-                return true
-            }
-            return false
-
-        }catch(err){
-            console.log(err)
-            throw new Error(err.message)
-        }
+  async checkEmail(email, mobileNumber) {
+    try {
+      let emailCheck = await customerModel.find({
+        $or: [{ email: email }, { mobileNumber: mobileNumber }],
+      });
+      if (emailCheck.length > 1) {
+        return true;
+      }
+      return false;
+    } catch (err) {
+      console.log(err);
+      throw new Error(err.message);
     }
-    async saveUser(email,name,hashPassword,mobileNumber){
-        try{
-            const user =new customerModel({
-                email:email,
-                name:name,
-                password:hashPassword,
-                mobileNumber:mobileNumber,
-            })
-            const newUser=await user.save()
-            console.log(newUser,"=============================>")
-            return newUser
-
-        }catch(err){
-            console.log(err)
-            throw new Error(err.message);
-        }
-
+  }
+  async saveUser(email, name, hashPassword, mobileNumber) {
+    try {
+      const user = new customerModel({
+        email: email,
+        name: name,
+        password: hashPassword,
+        mobileNumber: mobileNumber,
+      });
+      const newUser = await user.save();
+      // console.log(newUser,"=============================>")
+      return newUser;
+    } catch (err) {
+      console.log(err);
+      throw new Error(err.message);
     }
-  
-  
- 
-   
+  }
 }
 
 module.exports = new profileQuery();
