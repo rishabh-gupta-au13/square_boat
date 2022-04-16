546b3f07d35a1e8b076dc6270a0e13c807589879
rishabh-gupta-au13 <rgp02071993@gmail.com>
2022-04-16 15:50:27 +0530

    removed .env file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const cors = require('cors');
 const helmet = require('helmet');
 const app=express();
-const port=process.env.port || 3000
+const port=process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true, limit: '25mb' }))
 app.use(express.json())
 app.use(morgan('dev'))
